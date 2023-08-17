3191be01ea476f4b7f77af47b2bef5f8be0de49b
Skro <Skro@DESKTOP-84Q4EBJ>
2023-08-17 18:17:09 +0200

    Add project files.

---
+++ gigacigan/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Tabela from './tabela';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Tabela text = "disi gigo kurac"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ gigacigan/src/tabela.js
@@ -0,0 +1,29 @@
+import React, { Fragment } from "react";
+import classes from './tabela.module.css';
+
+const Tabela = props => {
+    return(
+        <Fragment>
+            <h1>{props.text}</h1>
+            <table className={classes.table}>
+                <tr>
+                <th>Company</th>
+                <th>Contact</th>
+                <th>Country</th>
+            </tr>
+            <tr>
+                <td>Alfreds Futterkiste</td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+            </tr>
+            <tr>
+                <td>Centro comercial Moctezuma</td>
+                <td>Francisco Chang</td>
+                <td>Mexico</td>
+            </tr>
+        </table>
+        </Fragment>
+    )
+}
+
+export default Tabela;
